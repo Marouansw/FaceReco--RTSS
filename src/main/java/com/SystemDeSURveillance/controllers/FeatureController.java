a29585d297825fc7a5fed50ea45bd78414e75b71
Marouan CHOTTA <marouan.chotta@gmail.com>
2024-11-21 16:16:04 +0100

    spring boot app 1

---
+++ src/main/java/com/SystemDeSURveillance/controllers/FeatureController.java
@@ -0,0 +1,24 @@
+package com.SystemDeSURveillance.controllers;
+
+import com.SystemDeSURveillance.service.FeatureConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FeatureController {
+
+    @Autowired
+    private FeatureConsumer featureConsumer;
+
+    @GetMapping("/Matching_results")
+    
+    public String getResult(Model model) throws InterruptedException {
+       
+        String lastMessage = featureConsumer.getLastConsumedMessage();
+        
+        model.addAttribute("message", lastMessage);
+        return "matching_results";
+    }
+}
